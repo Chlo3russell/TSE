c9fdbb0ff89b130bcaf9ec20d094316ca9c2b074
spicyyy12 <158062048+spicyyy12@users.noreply.github.com>
2025-02-13 15:04:57 +0000

    Update DatabaseExample.py
    added whois import

--- Snippets/DatabaseExample.py
+++ Snippets/DatabaseExample.py
@@ -1,10 +1,11 @@
+import whois
 import sqlite3
 
 # Create/connect to a database
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
-# Create a simple table
+# Create ISP table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS ISP (
         ISP_ID INT,
@@ -13,6 +14,7 @@ cursor.execute('''
     )
 ''')
 
+# Create Location table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Location (
         Location_ID INTEGER PRIMARY KEY,
@@ -22,6 +24,7 @@ cursor.execute('''
     )
 ''')
 
+# Create IP_Traffic table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS IP_Traffic (
         IP_Address VARCHAR(45) PRIMARY KEY,
@@ -34,6 +37,7 @@ cursor.execute('''
     )
 ''')
 
+# Create Flagged_Metrics table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Flagged_Metrics (
         Metric_ID INT PRIMARY KEY,
@@ -46,5 +50,54 @@ cursor.execute('''
     )
 ''')
 
-# Always commit changes
+#create a whois table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Whois (
+        Whois_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Website TEXT,
+        Registering_Company TEXT,
+        Date_Created TEXT,
+        Date_Expires TEXT,
+        DNS_Servers TEXT
+    )
+''')
+
 conn.commit()
+
+#whois function
+def get_whois_info(domain):
+    try:
+        w = whois.whois(domain)
+        print(f"WHOIS data retrieved: {w}")
+        return w
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+#saving data
+def save_to_db(domain, whois_info):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    
+    #inserting data
+    cursor.execute('''
+        INSERT INTO Whois (Website, Registering_Company, Date_Created, Date_Expires, DNS)
+        VALUES (?, ?, ?, ?, ?)''',
+                   (domain, whois_info.registrar, whois_info.creation_date, whois_info.expiration_date, ','.join(whois_info.name_servers)))
+    
+    conn.commit()
+    print(f"WHOIS info for {domain} inserted into database.")
+    conn.close()
+
+#main
+if __name__ == "__main__":
+    domain = "https://youtube.com"
+    whois_info = get_whois_info(domain)
+    
+    if whois_info:
+        save_to_db(domain, whois_info)
+        print(f"WHOIS info for {domain} saved to database.")
+    else:
+        print(f"Could not retrieve WHOIS info for {domain}.")
+
+    input("ENTER TO EXIT")
