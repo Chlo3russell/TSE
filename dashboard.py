a41cf746278a420ee0a78b0432fbb4ec1c49d37f
mayye35 <144482832+mayye35@users.noreply.github.com>
2025-04-01 00:46:25 +0100

    Added plans for initial routes

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import sqlite3
 
 app = Flask(__name__)
@@ -26,11 +26,18 @@ def dashboard():
     return render_template('dashboard.html', data=data)
 
 @app.route("/defense-settings")
-def defense():
-    # Get all the current threshold data 
-    return render_template('defense.html', data=data)
+def defense_settings():
+    # Get all the current threshold data... will write soon
 
+    # Display defense page
+    return render_template('defense.html') # add - data=data
 
+@app.route("/update-thresholds", method=['POST'])
+def update_thresholds():
+    # Get new threshold limit
+    new_threshold = request.form.get('limit')
+    # Redirect to defense page
+    return redirect(url_for("defense_settings"))
 
 if __name__ == "__main__":
     app.run(debug=True)
