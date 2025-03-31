05189aec27739f55aae08516b5df735c1c3f5f86
Chloe Russell <chloerussell2017@gmail.com>
2025-03-31 09:07:24 +0100

    Changed some names
    And added some extra functionality

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 import threading
 import os
 
+# This script runs two threads: one for the Flask web application 
+# and another for the traffic monitoring script.
+
+# This just reduces the amount of scrips we have to run manually.
+
+
 def run_flask():
-    os.system("python targetWebsite/app.py")  # Runs your fake website
+    os.system("python targetWebsite/targetWebsite.py") 
 
 def run_monitoring():
-    os.system("python trafficMonitor.py")  # Runs the traffic monitor
+    os.system("python trafficMonitor.py")  
 
 threading.Thread(target=run_flask).start()
 threading.Thread(target=run_monitoring).start()
