c08c167681f67a164562e59755e764dcb34a81b0
mayye35 <144482832+mayye35@users.noreply.github.com>
2025-04-21 19:59:04 +0100

    Logger file is updated for central logging for all files, all files will call on this one for insantating the logging messages

--- logger.py
+++ logger.py
@@ -2,12 +2,13 @@ import logging
 from logging.handlers import RotatingFileHandler
 import logging.handlers
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
 
 # Constants - log directory and log file that all logs across all files will go into
 LOG_DIRECTORY = "logs"
 LOG_FILE = os.path.join(LOG_DIRECTORY, "app.log")
+
 
 def setup_logger(name=__name__):
     '''
@@ -25,26 +26,70 @@ def setup_logger(name=__name__):
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     # Handling multiple log files - when a log file reaches a certain limit it will create backups (max of 3 of size 1MB)
-    if not logging.handlers:
-        handler = RotatingFileHandler(LOG_FILE, maxBytes=1000000, backupCount=3, encoding='utf-8')
+    if not logger.handlers:
+        handler = RotatingFileHandler('logs/app.log', maxBytes=1000000, backupCount=3, encoding='utf-8')
         handler.setLevel(logging.INFO)
         handler.setFormatter(formatter)
         logger.addHandler(handler)
+    
+    return logger
 
 def format_log_data(log_entry):
     '''
     Helper function to format the log entry into structured, user readable data using regular expressions.
     '''
-    pass
+    pattern = r"^(?P<timestamp>[\d\-]+\s[\d:,]+) - (?P<logger>[^ ]+) - (?P<level>[A-Z]+) - (?P<message>.*)"
+    match = re.match(pattern, log_entry)
+    if match:
+        timestamp = datetime.strptime(match['timestamp'], '%Y-%m-%d %H:%M:%S,%f')
+        return {
+            'timestamp' : timestamp,
+            'logger' : match['logger'],
+            'level' : match['level'],
+            'message' : match['message'].strip()
+        }
+    return None
 
 def get_all():
     '''
     Retrieve all logs from the log file, call the formatter helper function and return structured log data.
     '''
-    pass
+    logs = []
+    
+    if not os.path.exists(LOG_FILE):
+        return []
+
+    with open(LOG_FILE, 'r') as lf:
+        for log in lf:
+            log_data = format_log_data(log)
+            if log_data:
+                logs.append(log_data)
+    lf.close()
+    return logs
 
 def filter_by_time(start, end):
     '''
     Retrieve all logs from the log file within a certain timeframe.
     '''
-    pass
+    return [log for log in get_all() if start <= log['timestamp'] <= end]
+
+def filer_by_level(level):
+    '''
+    Retrieve all logs from the log file that match a certain level.
+    '''
+    return [log for log in get_all() if log['level'] == level.upper()]
+
+if __name__ == "__main__":
+    logger = setup_logger(__name__)
+    logger.info("Logger test message")
+
+    start = datetime.now() - timedelta(hours=1)
+    end = datetime.now()
+    recent_logs = filter_by_time(start, end)
+
+    print("\nRecent Logs:")
+    for log in recent_logs:
+        print(log)
+    
+    print("\nStructured Example:")
+    print(recent_logs[0] if recent_logs else "None Found")
